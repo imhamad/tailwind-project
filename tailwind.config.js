3a0e79a654f979ec3d8b82171ad97a001b4872df
Hamad <hamad.coder@gmail.com>
2023-06-30 00:41:30 +0500

    Updates Performed:: savepoint ~ Hamad

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// comment in js
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    // exporting content
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+
+}
+
+// Now command for NPX
+
+// npx tailwindcss -i dist/input.css -o src/output.css
